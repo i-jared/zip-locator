5c6c8469a20056de36b44b18eb7c1a8a5e636d77
i-jared <32687987+i-jared@users.noreply.github.com>
2026-03-11 13:50:25 -0600

    afd loc

--- utils.ts
+++ utils.ts
@@ -35,4 +35,7 @@ export const TARGET_LOCATIONS: LocationTarget[] = [
   { id: 'everett', name: 'Everett', lat: 47.907556185455604, lng: -122.23426400229107 },
   { id: 'vancouver', name: 'Vancouver', lat: 45.65842399116114, lng: -122.59338621772888 },
   { id: 'kirkland', name: 'Kirkland', lat: 47.70997319790552, lng: -122.19003647346379 },
+  { id: 'meridian-gentry-way', name: '3080 E Gentry Way #200, Meridian', lat: 43.5983436, lng: -116.3567336 },
+  { id: 'park-city-center-drive', name: '1122 Center Dr Ste D-350, Park City', lat: 40.7201349, lng: -111.5390464 },
+  { id: 'slc-3000-east', name: '6360 S 3000 E Ste 2000, SLC', lat: 40.6339463, lng: -111.8067671 },
 ];
